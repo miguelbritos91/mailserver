7a1d5087fbf5496fdde6abe33e19bf64c307bb5e
Miguel Britos <miguelbritos91@gmail.com>
2021-03-29 21:58:58 -0300

    Config cors

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'https://style-web.net/')
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')

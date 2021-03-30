484e7b9db31de57d2095c762e899a4ebd0692548
Miguel Britos <miguelbritos91@gmail.com>
2021-03-29 21:56:32 -0300

    Cors allow origin

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const nodemailer = require('nodemailer')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3500
 
 // Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
+    next()
+}) 
 
 app.post("/mail/send", (req, res)=>{
     console.log("Email enviado");
